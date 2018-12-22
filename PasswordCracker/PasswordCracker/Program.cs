2783fdc94c5e49ab340aa60aa86b6da4853631f7
Matt Dean <mattdean98@gmail.com>
2018-12-22 08:57:54 -0600

    started  setup

---
+++ PasswordCracker/PasswordCracker/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordCracker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var entry = "";
+            Console.WriteLine("Welcome to Password Cracker, made by Matt Dean as a side project for educational purposes only.");
+            do {
+                Console.Write("Do you want to generate 100 passwords? (Y/N): ");
+                entry = Console.ReadLine();
+                if (entry.ToUpper() == "Y")
+                {
+                    GeneratePasswords();
+                }
+                else if (entry.ToUpper() == "N")
+                {
+                    //get user to input a password
+                }
+            }
+            while (entry.ToUpper() != "Y" && entry.ToUpper() != "N");
+            Console.Write("Enter the format of the password you want to crack (\"l\" for letter and \"n\" for number, example \"llllnn\"): ");
+            CrackPassword(Console.ReadLine());
+        }
+        static void GeneratePasswords()
+        {
+
+        }
+        static void CrackPassword(string format)
+        {
+
+        }
+    }
+}
